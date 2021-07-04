448f405d0302d367e81d3208d005e8044888e24f
markusperganda <85565182+markusperganda@users.noreply.github.com>
2021-07-04 23:05:14 +0700

    ini commit git

---
+++ src/latihanwebinar/LatihanWebinar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanwebinar;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class LatihanWebinar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Laporan Webinar nich...");
+        // TODO code application logic here
+    }
+}
